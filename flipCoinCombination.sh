7ae3f2fd9f9a791cd553e10b607eef7430b04af5
saurav004 <logtosaurav@gmail.com>
2020-10-10 09:26:34 +0530

    ADDED:code to simulate coin flip and print outcome

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,16 @@
 
 echo "-------------------------------------------------------------------------WELCOME TO FLIP COIN COMBINATION--------------------------------------------------------------------------"
 
+#HEADS AND TAILS ARE GIVEN VALUES 1 AND 0 REPECTIVELY
+HEADS=1
+TAILS=0
+coinSaid=$((RANDOM%2))
+
+#CHECKING IF HEAD WON OR TAIL WON 
+
+if [ $coinSaid == $HEADS ]
+then
+echo "HEADS WON!!!!!!"
+else
+echo "TAILS WON!!!!!!"
+fi
