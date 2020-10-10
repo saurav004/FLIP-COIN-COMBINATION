b6322af80e9dc49fce564f076408b9993ee32f53
saurav004 <logtosaurav@gmail.com>
2020-10-10 23:26:47 +0530

    code to Sort SINGLETS DOUBLETS and TRIPLETS

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,44 +1,42 @@
 #! /bin/bash -x
 
-echo "-------------------------------------------------------------------------WELCOME TO FLIP COIN COMBINATION--------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------WELCOME TO FLIP COIN COMBINATION-------------------------------------------------------------------------------"
 
 #HEADS AND TAILS ARE GIVEN VALUES 1 AND 0 REPECTIVELY
 HEADS=1
 TAILS=0
-
+H=0
+T=0
 #DICTIONARY CREATED
+
 declare -A DICT
+
 
 read -p "Enter no of times to toss the coin" num
 
-flips=3
-#TRIPLET COMBINATION CODE
-function coinFlip() {
+
+echo  $num $flips
+
+function coinFlip(){
 for (( i=1; i<=$num; i++ ))
 do
+read -p "for $i combination choose 1.SINGLET 2.DOUBLET 3.TRIPLETS" flips
 str=""
- for(( j=0; j<$flips; j++ ))
- do
-   random=$(( RANDOM % 2 ))
-	if [ $random -eq 1 ]
-	then
-	   str=$str"H"
-	else
-            str=$str"T"
-	fi
-  done
-echo $str
-DICT["$str"]=$(( $((DICT["$str"])) + 1 ))
+for (( j=1; j<=$flips; j++ ))
+do
+random=$(( RANDOM % 2 ))
+if [ $random -eq 1 ]
+then
+str=$str"H"
+else
+str=$str"T"
+fi
+done
+echo  $str
+DICT[$str]=$(( DICT[$str] + 1 ))
 done
 }
 
 coinFlip
-echo "${!DICT[@]} : ${DICT[@]}"
 
-for i in ${!DICT[@]}
-do
-	store=$(($((DICT[$i]))*100))
-	percentage=$(($store/$num))
-	echo "occurance percentage of $i:$percentage"
-done
-
+echo $(printf "%s\n" ${!DICT[@]} : ${DICT[@]} | sort -n)
