a67d57f0badd160ba380225e0a16a037998b27eb
saurav004 <logtosaurav@gmail.com>
2020-10-10 18:00:43 +0530

    ADDED:code to store coin flip results in dictionary and calculate percent

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,13 +5,26 @@ echo "-------------------------------------------------------------------------W
 #HEADS AND TAILS ARE GIVEN VALUES 1 AND 0 REPECTIVELY
 HEADS=1
 TAILS=0
-coinSaid=$((RANDOM%2))
 
-#CHECKING IF HEAD WON OR TAIL WON 
+#DICTIONARY CREATED
+declare -A DICT
 
-if [ $coinSaid == $HEADS ]
-then
-echo "HEADS WON!!!!!!"
-else
-echo "TAILS WON!!!!!!"
-fi
+read -p "Enter no of times to toss the coin" num
+
+for ((i=0; i<$num; i++))
+do
+	coinSaid=$((RANDOM%2))
+	if [ $coinSaid == $HEADS ]
+	then
+		DICT[$HEADS]=$((DICT[$HEADS]+1))
+	else
+		DICT[$TAILS]=$((DICT[$TAILS]+1))
+	fi
+done
+
+percentH=$(($((DICT[$HEADS]))*100))
+percentHeads=$(($percentH/$num))
+percentT=$(($((DICT[$TAILS]))*100))
+percentTails=$(($percentT/$num))
+echo ${DICT[@]}
+echo "Head percent=$percentHeads % and Tails percent = $percentTails % "
